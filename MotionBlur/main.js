3a9a244c69dd9674707eb431fb736656484d6873
schaffsp <89745868+schaffsp@users.noreply.github.com>
2022-05-08 11:05:45 -0400

    Initial Upload Based on Moon Lab

---
+++ MotionBlur/main.js
@@ -0,0 +1,280 @@
+import {VertexAttributes} from './vertex-attributes';
+import {ShaderProgram} from './shader-program';
+import {VertexArray} from './vertex-array';
+import {Matrix4} from './matrix';
+import {Vector4, Vector3} from './vector';
+import {Camera} from './camera';
+
+let rotationSpeed = .2;
+let movementSpeed = .5;
+let canvas;
+let groundAttributes;
+let shaderProgram;
+let groundVao;
+let moonVao;
+let clipFromEye;
+let grassTexture;
+let strafe = 0;
+let forward = 0;
+let camera;
+
+async function readImage(url) {
+  const image = new Image();
+  image.src = url;
+  await image.decode();
+  return image;
+}
+
+function createTexture2d(image, textureUnit) {
+  gl.activeTexture(textureUnit);
+  const texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+  gl.generateMipmap(gl.TEXTURE_2D);
+  return texture;
+}
+
+function render() {
+  gl.viewport(0, 0, canvas.width, canvas.height);
+  gl.clearColor(0.6, 0.6, 0.9, 1);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  shaderProgram.bind();
+
+  shaderProgram.setUniformMatrix4('clipFromEye', clipFromEye);
+  shaderProgram.setUniformMatrix4('eyeFromWorld', camera.eyeFromWorld);
+  shaderProgram.setUniformMatrix4('worldFromModel', Matrix4.identity());
+  shaderProgram.setUniform1i('grassTexture', 0);
+  groundVao.bind();
+  groundVao.drawIndexed(gl.TRIANGLES);
+  groundVao.unbind();
+  shaderProgram.setUniform1i('grassTexture', 1);
+  moonVao.bind();
+  moonVao.drawIndexed(gl.TRIANGLES);
+  moonVao.unbind();
+
+  shaderProgram.unbind();
+}
+
+function onResizeWindow() {
+  canvas.width = canvas.clientWidth;
+  canvas.height = canvas.clientHeight;
+  clipFromEye = Matrix4.fovPerspective(45, canvas.width / canvas.height, 0.1, 1000);
+  render();
+}
+
+function generateSphere(nlatitudes, nlongitudes, radius) {
+  const positions = [];
+  const normals = [];
+  const indices = [];
+  const texPositions = [];
+
+  const seedPositions = [];
+  for (let ilatitude = 0; ilatitude < nlatitudes; ++ilatitude) {
+    const radians = ilatitude / (nlatitudes - 1) * Math.PI - Math.PI / 2;
+    const x = radius * Math.cos(radians);
+    const y = radius * Math.sin(radians);
+    seedPositions.push(Vector4.notConstructor(x, y, 0, 1));
+  }
+
+  for (let ilongitude = 0; ilongitude < nlongitudes; ++ilongitude) {
+    const degrees = ilongitude / (nlongitudes - 1) * 360;
+    const rotater = Matrix4.rotateY(degrees);
+    //console.log('rotater', rotater);
+    for (let ilatitude = 0; ilatitude < nlatitudes; ++ilatitude) {
+      const p = rotater.multiplyVector(seedPositions[ilatitude]);
+      positions.push(p.x , p.y +10 , p.z);
+      texPositions.push((ilongitude / (nlongitudes - 1)), (ilatitude / (nlatitudes - 1)));
+      const normal = p.normalize();
+      normals.push(normal.x, normal.y, normal.z);
+    }
+  }
+
+  console.log('texPosition first and last', texPositions[0] , texPositions[1], texPositions[texPositions.length-2], texPositions[texPositions.length-1]);
+
+  for (let ilongitude = 0; ilongitude < nlongitudes; ++ilongitude) {
+    const iNextLongitude = (ilongitude + 1) % nlongitudes;
+    for (let ilatitude = 0; ilatitude < nlatitudes - 1; ++ilatitude) {
+      const iNextLatitude = (ilatitude + 1) % nlatitudes;
+      indices.push(
+        ilongitude * nlatitudes + ilatitude,
+        ilongitude * nlatitudes + iNextLatitude,
+        iNextLongitude * nlatitudes + iNextLatitude,
+      );
+      indices.push(
+        ilongitude * nlatitudes + ilatitude,
+        iNextLongitude * nlatitudes + iNextLatitude,
+        iNextLongitude * nlatitudes + ilatitude,
+      );
+    }
+  }
+
+  //console.log(positions);
+  
+  const attributes = new VertexAttributes();
+  attributes.addAttribute('texPosition', nlatitudes * nlongitudes, 2, texPositions);
+  attributes.addAttribute('position', nlatitudes * nlongitudes, 3, positions);
+  attributes.addAttribute('normal', nlatitudes * nlongitudes, 3, normals);
+  attributes.addIndices(indices);
+
+  return attributes;
+}
+
+async function initialize() {
+  canvas = document.getElementById('canvas');
+  window.gl = canvas.getContext('webgl2');
+
+  gl.enable(gl.CULL_FACE);
+  gl.enable(gl.DEPTH_TEST);
+
+  const grassImage = await readImage('grassTexture.jpeg');
+  createTexture2d(grassImage, gl.TEXTURE0);
+
+  const moonImage = await readImage('moonimage.jpg');
+  createTexture2d(moonImage, gl.TEXTURE1);
+
+
+  const positions = [
+    -100, -0.5,  100,
+     100, -0.5,  100,
+    -100, -0.5, -100,
+     100, -0.5, -100,
+  ];
+
+  const texPositions = [
+      0.2, 0.2,
+      0.8, 0.2,
+      0.8, 0.8,
+      0.2, 0.8, 
+  ];
+
+  const normals = [
+    0, 1, 0,
+    0, 1, 0,
+    0, 1, 0,
+    0, 1, 0,
+  ];
+
+  const indices = [
+    0, 1, 3,
+    0, 3, 2,
+  ];
+  let moonAttributes = generateSphere(20, 20, 10);
+
+  groundAttributes = new VertexAttributes();
+  groundAttributes.addAttribute('position', 4, 3, positions);
+  groundAttributes.addAttribute('texPosition', 4, 2, texPositions);
+  groundAttributes.addAttribute('normal', 4, 3, normals);
+  groundAttributes.addIndices(indices);
+
+  const vertexSource = `
+uniform mat4 clipFromEye;
+uniform mat4 eyeFromWorld;
+uniform mat4 worldFromModel;
+
+in vec3 position;
+in vec2 texPosition;
+in vec3 normal;
+
+out vec3 mixNormal;
+out vec2 mixTexPosition;
+
+void main() {
+  gl_PointSize = 3.0;
+  gl_Position = clipFromEye * eyeFromWorld * worldFromModel * vec4(position, 1.0);
+  mixNormal = (eyeFromWorld * worldFromModel * vec4(normal, 0.0)).xyz;
+  mixTexPosition = texPosition;
+}
+  `;
+
+  const fragmentSource = `
+uniform sampler2D grassTexture;
+
+const float ambientFactor = 0.5;
+const vec3 lightDirection = normalize(vec3(0.0, 1.0, 0.0));
+const vec3 albedo = vec3(0.8, 0.8, 0.8);
+
+in vec3 mixNormal;
+in vec2 mixTexPosition;
+
+out vec4 fragmentColor;
+
+void main() {
+  vec3 normal = normalize(mixNormal);
+  /*float litness = max(0.0, dot(normal, lightDirection));
+  vec3 diffuse = albedo * litness * (1.0 - ambientFactor);
+  vec3 ambient = albedo * ambientFactor;
+  fragmentColor = vec4(diffuse + ambient, 1.0);*/
+  fragmentColor = texture(grassTexture, mixTexPosition);
+}
+  `;
+
+  camera = new Camera(Vector3.fromValues(0, 0, -10), Vector3.fromValues(0, 0, 0), Vector3.fromValues(0, 1, 0));
+
+  shaderProgram = new ShaderProgram(vertexSource, fragmentSource);
+  groundVao = new VertexArray(shaderProgram, groundAttributes);
+  moonVao = new VertexArray(shaderProgram, moonAttributes);
+
+  window.addEventListener('resize', onResizeWindow);
+  onResizeWindow();
+
+  canvas.addEventListener('pointerdown', event => {
+    document.body.requestPointerLock();
+  });
+
+  window.addEventListener('resize', onResizeWindow);
+  window.addEventListener('pointerdown', () => {
+    document.body.requestPointerLock();
+  });
+  
+  window.addEventListener('pointermove', event => {
+    //console.log("This");
+    if (document.pointerLockElement) {
+      //console.log(-event.movementY);
+      camera.yaw(-event.movementX * rotationSpeed);
+    //  console.log("YAW YEET");
+      camera.pitch(-event.movementY * rotationSpeed);
+      render();
+    }
+  });
+
+  window.addEventListener('keydown', event => {
+    if(event.key === 'w') {
+      forward = 1;
+    } else if(event.key === 'a') {
+      strafe = -1;
+    } else if(event.key === 's') {
+      forward = -1;
+    } else if(event.key === 'd') {
+      strafe = 1;
+    }
+  });
+
+  window.addEventListener('keyup', event => {
+    if(event.key === 'w' || event.key === 's') {
+      forward = 0;
+    } else if(event.key === 'a' || event.key === 'd') {
+      strafe = 0;
+    }
+  });
+
+  move();
+  onResizeWindow();
+}
+
+function move() {
+  //console.log("get ready to move it move it");
+  if (forward != 0) {
+    //console.log("moving forward.");
+    camera.advance(movementSpeed * forward);
+    render();
+  } 
+  if (strafe != 0) {
+    //console.log("To the left to the left to the right to the right\nOne hop this time");
+    camera.strafe(movementSpeed * strafe);
+    render();
+  }
+  requestAnimationFrame(move);
+};
+
+window.addEventListener('load', initialize);
---
+++ MotionBlur/vector.js
@@ -0,0 +1,217 @@
+export class Vector4{
+
+    constructor() {
+        this.v_array = [0, 0, 0, 0];
+    }
+
+    static fromValues(x, y, z) {
+        let vec = new Vector4();
+        vec.set(0, x);
+        vec.set(1, y);
+        vec.set(2, z);
+        vec.set(3, 1);
+        return vec;
+    }
+
+    static notConstructor(x, y, z) {
+        let vec = new Vector4();
+        vec.set(0, x);
+        vec.set(1, y);
+        vec.set(2, z);
+        vec.set(3, 1);
+        return vec;
+    }
+
+    get x() {
+        return this.v_array[0];
+    }
+
+    get y() {
+        return this.v_array[1];
+    }
+
+    get z() {
+        return this.v_array[2];
+    }
+
+    get h() {
+        return this.v_array[3];
+    }
+
+    static notConstructorArray(arr) {
+        if (arr.length < 4) {
+            return
+        }
+        
+        let out = new Vector4();
+        
+        for (let i = 0; i < 3; i++) {
+            out.set(i, arr[i]);
+        }
+        out.set(3, 1);
+
+        return out
+    }
+
+    get(index) {
+        return this.v_array[index];
+    }
+
+    getX() {
+        return this.v_array[0];
+    }
+
+    getY() {
+        return this.v_array[1];
+    }
+
+    getZ() {
+        return this.v_array[2];
+    }
+
+    set(index, value) {
+        this.v_array[index] = value;
+    }
+
+    divide(scalar) {
+        this.set(0, this.get(0) / scalar);
+        this.set(1, this.get(1) / scalar);
+        this.set(2, this.get(2) / scalar);
+        this.set(3, this.get(3) / scalar);
+    }
+
+    magnitude() {
+        return Math.sqrt(Math.abs(this.get(0) * this.get(0) + this.get(1) * this.get(1) + this.get(2) * this.get(2) + this.get(3) * this.get(3)));
+    }
+
+    normalize() {
+        this.divide(this.magnitude());
+        return this;
+    }
+
+    dot(other) {
+        return this.get(0) * other.get(0) + this.get(1) * other.get(1) + this.get(2) * other.get(2) + this.get(3) * other.get(3);
+    }
+
+    toVector3(){
+        return Vector3.fromValues(this.get(0), this.get(1), this.get(2));
+    }
+}
+
+export class Vector3 {
+    constructor() {
+        this.v_array = [0, 0, 0]
+    }
+
+    static notConstructor(x, y, z) {
+        let vec = new Vector3();
+        vec.setX(x);
+        vec.setY(y);
+        vec.setZ(z);
+        return vec;
+    }
+
+    static fromArray(array) {
+        if (array.length != 3) return;
+        let new_vector = new Vector3();
+        new_vector.setX(array[0]);
+        new_vector.setY(array[1]);
+        new_vector.setZ(array[2]);
+        return new_vector;
+    }
+
+    static fromValues(x, y, z) {
+        let new_vector = new Vector3();
+        new_vector.setX(x);
+        new_vector.setY(y);
+        new_vector.setZ(z);
+        return new_vector;
+    }
+
+    flatten() {
+        return this.v_array;
+    }
+
+    divide(scalar) {
+        this.setX(this.getX() / scalar);
+        this.setY(this.getY() / scalar);
+        this.setZ(this.getZ() / scalar);
+    }
+
+    magnitude() {
+        return Math.sqrt(this.getX() ** 2 + this.getY() ** 2 + this.getZ() ** 2);
+    }
+
+    normalize() {
+        let m = this.magnitude();
+        return Vector3.fromValues(
+            this.getX() / m,
+            this.getY() / m,
+            this.getZ() / m
+        );
+    }
+
+    subtract(other) {
+        return Vector3.fromValues(this.getX() - other.getX(), this.getY() - other.getY(), this.getZ() - other.getZ());
+    }
+    
+    add(other) {
+        return Vector3.fromValues(this.getX() + other.getX(), this.getY() + other.getY(), this.getZ() + other.getZ());
+    }
+
+    dot(other) {
+        return this.getX() * other.getX() + this.getY() * other.getY() + this.getZ() * other.getZ();
+    }
+
+    cross(other) {
+        return Vector3.fromValues(
+            this.getY() * other.getZ() - this.getZ() * other.getY(),
+            this.getZ() * other.getX() - this.getX() * other.getZ(),
+            this.getX() * other.getY() - this.getY() * other.getX()
+        );
+    }
+
+    toVector4() {
+        return Vector4.notConstructor(this.getX(), this.getY(), this.getZ(), 1);
+    }
+
+    get x() {
+        return this.v_array[0];
+    }
+
+    get y() {
+        return this.v_array[1];
+    }
+
+    get z() {
+        return this.v_array[2];
+    }
+
+    get h() {
+        return this.v_array[3];
+    }
+
+    getX() {
+        return this.v_array[0];
+    }
+
+    getY() {
+        return this.v_array[1];
+    }
+
+    getZ() {
+        return this.v_array[2];
+    }
+
+    setX(value) {
+        this.v_array[0] = value;
+    }
+
+    setY(value) {
+        this.v_array[1] = value;
+    }
+
+    setZ(value) {
+        this.v_array[2] = value;
+    }
+}
